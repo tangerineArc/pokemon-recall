aac24a9a201365ced2e377cd6c13b9e3eb5b38bb
Swagatam <swagatamheroz@outlook.com>
2025-01-29 18:54:03 +0530

    feat: random data fetching

--- src/utils/fetch-data.js
+++ src/utils/fetch-data.js
@@ -1,6 +1,13 @@
+const LIMIT = 1025;
+
 export default async function getSpritesData() {
+  const last = LIMIT - (20 - 1) * 3;
+  
+  const start = Math.floor(Math.random() * last) + 1;
+  const end = start + (20 - 1) * 3;
+
   const spritesData = [];
-  for (let i = 3; i <= 60; i += 3) {
+  for (let i = start; i <= end; i += 3) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
 
     const response = await fetch(url);
